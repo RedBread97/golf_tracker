d56ba5588e4dd737ada4cbbfd9fedaa875160b8e
abelzer09 <aaronrbelzer@gmail.com>
2022-05-04 10:41:43 -0500

    perparing for merege worked on routes

--- controllers/api/scoreRoutes.js
+++ controllers/api/scoreRoutes.js
@@ -2,13 +2,41 @@ const router = require('express').Router();
 const { Golfer, ScoreCard } = require('../../models');
 
 router.get('/', async (req, res) => {
-    try {
-        const scoreData = await ScoreCard.findAll({
-            include: [{ model: Golfer}]
-        });
-        res.status(200).json(scoreData);
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err);
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        try {
+            const scoreData = await ScoreCard.findAll({
+                include: [{ model: Golfer }]
+            });
+
+            const scorecard = scoreData.map((score) =>
+                score.get({ plain: true })
+            );
+
+            res.render('scorecard', {
+                scorecard,
+                loggedIn: req.sessionOptions.loggedIn,
+            });
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err);
+        }
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const dbScoreData = await ScoreCard.create({
+            course_name: req.body.coursename,
+            round_score: req.body.roundscore,
+            notes: req.body.notes
+        });
+        res.status(200).json(dbScoreData)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+
+    }
+});
+
