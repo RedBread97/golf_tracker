bd025574ddfd9f20279cab2a64d5e4c10cdfb9bb
abelzer09 <aaronrbelzer@gmail.com>
2022-05-04 14:58:09 -0500

    added cdn for model and form

--- public/js/score.js
+++ public/js/score.js
@@ -23,6 +23,15 @@ const newFormHandler = async (event) => {
     }
 };
 
+
+
 document
   .querySelector('.new-scorecard-form')
   .addEventListener('submit', newFormHandler);
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
