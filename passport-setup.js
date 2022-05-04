ad948495efdbf882fb3f664d6ad78f3c709555de
Hannah Lodge <lodg0021@umn.edu>
2022-05-04 10:37:11 -0500

    login created

--- passport-setup.js
+++ passport-setup.js
@@ -1,8 +1,12 @@
+const res = require('express/lib/response');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const models = require("./models")
+const Golfer = models.Golfer
+
 
 passport.serializeUser(function(user, done) {
-    done(null, user.id); //Databas
+    done(null, user.id); //Database
 });
 
 passport.deserializeUser(function(id, done) {
@@ -20,7 +24,28 @@ passport.use(new GoogleStrategy ({
 }, 
 function(accessToken, refreshToken, profile, done) {
     //use profile info (profile id) to check if the user is registered in the DB
-    User.findOrCreate({ googleId: profile.id}, function (err, user) {
-        return done(null, user);
-    });
+    // Golfer.findOrCreate({ googleId: profile.id} function (err, user) {
+    //     return done(null, user);
+    // });
+    Golfer.findOne({where: {googleId: profile.id}})
+    .then((golferInfo)=> {
+        console.log(profile.id)
+        console.log (profile.displayName)
+        console.log (profile)
+        if(!golferInfo){
+            Golfer.create({googleId:profile.id, 
+                golferName:profile.displayName,
+                email: profile.emails[0].value
+             }).then((profileInfo)=>{
+                 done(null, profileInfo)
+             }).catch((err)=>{
+                 console.log(err)
+                 done(err, null)
+                })
+        } else{
+            done(null, golferInfo)
+        }
+    }).catch((error)=>{
+        done(error, null)
+    })
 }));
